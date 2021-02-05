7c5d2280dec7cd09553b9b408b765517e60e0020
Pavel Khan <Pavelkhanoff@gmail.com>
2021-02-05 14:44:32 +0600

    after 1st reviews all done

--- homework.py
+++ homework.py
@@ -16,16 +16,26 @@ PRAKTIKUM_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-print(TELEGRAM_TOKEN)
+TIMEOUT = 5
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
+    try:
+        homework_name = homework['homework_name']
+    except KeyError:
+        logging.exception(msg='Ключа "homework_name" не найдено')
+    except Exception:
+        logging.exception(msg='Что-то пошло не так c ключом "homework_name"')
     if homework['status'] == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
+    elif homework['status'] == 'approved':
         verdict = ('Ревьюеру всё понравилось, '
                    'можно приступать к следующему уроку.')
+    elif homework['status'] == 'reviewing':
+        verdict = ('Ревьюеру всё понравилось, '
+                   'можно приступать к следующему уроку.')
+    else:
+        verdict = 'Пришел непонятный статус дз'
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
@@ -40,9 +50,10 @@ def get_homework_statuses(current_timestamp):
         homework_statuses = requests.get(
             PRAKTIKUM_URL, params=params, headers=headers)
         return homework_statuses.json()
-    except ValueError:
-        logging.exception(msg='Ошибка в работе json')
-    except ConnectionError:
+    except UnicodeDecodeError:
+        logging.exception(msg='Ошибка при восстановлении данных '
+                              'из бит-последовательности')
+    except requests.RequestException:
         logging.exception(msg='Ошибка соединения')
     except Exception:
         logging.exception(msg='Что-то сломалось:)')
@@ -64,12 +75,12 @@ def main():
                 send_message(parse_homework_status(
                     new_homework.get('homeworks')[0]), bot_client)
             current_timestamp = new_homework.get(
-                'current_date', current_timestamp)  # обновить timestamp
+                'current_date', int(time.time()))  # обновить timestamp
             time.sleep(300)  # опрашивать раз в пять минут
 
         except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
-            time.sleep(5)
+            logging.exception(msg=f'Бот столкнулся с ошибкой: {e}')
+            time.sleep(TIMEOUT)
 
 
 if __name__ == '__main__':
